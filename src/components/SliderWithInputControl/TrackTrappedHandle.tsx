de56449ce7f86a45a891c3b415baec5c3d1cf564
Amith <agusamith@gmail.com>
2025-01-04 14:00:52 +0530

    fix: build issue

--- src/components/SliderWithInputControl/TrackTrappedHandle.tsx
+++ src/components/SliderWithInputControl/TrackTrappedHandle.tsx
@@ -2,7 +2,7 @@ import { SliderProps } from "rc-slider";
 import { cloneElement } from "react";
 
 // A slider handle which is confined within the track , no overflows
-export const TrackTrappedHandle: SliderProps['handleRender'] = (node, props) => {
+export const TrackTrappedHandle: SliderProps['handleRender'] = (node) => {
     return cloneElement(node,{style:{
         ...node.props.style,
         transform:`translateX(-${ node.props.style?.left })`
